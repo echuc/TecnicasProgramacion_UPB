1b03fca50336f0225fc37f40e6d87de1d6fb36da
Lic. Esdras Chuc <qroomexico@yahoo.com.mx>
2013-05-21 02:02:59 -0500

    Formulario Actualizado

--- formulario.php
+++ formulario.php
@@ -6,25 +6,27 @@
 </head>
 <body>
 	<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-		Registro de usuario
-		Apellido : <input type=text name="apellido">
-		Nombre : <input type=text name="nombre">
+		<h1>Registro de usuario</h1>
+		Nombre : <input type=text name="nombre"><br>
+		Edad : <input type=text name="edad"><br>
 		Género :		
 			Hombre <input type=radio name="genero" value="H">
-			Mujer <input type=radio name="genero" value="M">
-		Ocupación		
+			Mujer <input type=radio name="genero" value="M"><br>
+		Ocupación :		
 			<select name="ocupacion">
 				<option value="profesor">Profesor</option>
 				<option value="estudiante">Estudiante</option>
 				<option value="amaCasa">Ama de casa</option>
 				<option value="burocrata">Burócrata</option>
 				<option value="otro">Otro</option>
-			</select>
-		Comentarios
-			<textarea rows="3" name="comentarios">
-				Escriba aquí sus comentarios
-			</textarea>
-			<input type="submit" value="Enviar">
+			</select><br>
+		Aficiones :
+			<input type=checkbox name="aficion1" value="Deporte">Hacer deporte &nbsp;
+			<input type=checkbox name="aficion2" value="Leer">Leer&nbsp;
+			<input type=checkbox name="aficion3" value="Socializar">Socializar<br>
+		Comentarios :
+			<textarea rows="3" name="comentarios">Escriba aquí sus comentarios</textarea><br>
+			<input type="submit" value="Enviar">&nbsp;
 			<input type="reset" value="Nuevo">
 	</form>
 </body>
