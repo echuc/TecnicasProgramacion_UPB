78e3810ce7e815d4b1a8bfcb50058066194f9267
echuc <qroomexico@yahoo.com.mx>
2013-07-19 11:23:59 -0500

    update2

--- ordenamientoBurbujaconFunciones.php
+++ ordenamientoBurbujaconFunciones.php
@@ -1,3 +1,6 @@
+<!-- 
+Descripción : Ordemiento con el método de Burbuja usando funciones
+-->
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -29,23 +32,33 @@
 			echo $vector[$i]."<br>";
 		}
 
-		// Ordenamos el vector con el método de Burbuja
-		for ($i=0;$i<$tamano;$i++) {
-			for ($j=0;$j<$tamano-$i;$j++) {
-				// Comparamos los valores adyacentes
-				if ($vector[$j] > $vector[$j+1]) {
-					$intercambio = $vector[$j+1];
-					$vector[$j+1]= $vector[$j]; 
-					$vector[$j] = $intercambio;
-				}
-			}
-		}
+		// Llamar a la función de ordenamiento tipo burbuja
+		$vector = ordamientoBurbuja($vector,$tamano);
 
 		
 		echo "<h2>Vector ordenado</h2>";
 		for ($i=0;$i<=$tamano;$i++) {
 			echo $vector[$i]."<br>";
 		}	
+
+
+		// funcion que ordena un vector con el método de burbuja
+		// Parámetros $nombreVector y $tamano
+		function ordamientoBurbuja($nombreVector,$tamano)
+		{
+			for ($i=0;$i<$tamano;$i++) {
+				for ($j=0;$j<$tamano-$i;$j++) {
+					// Comparamos los valores adyacentes
+					if ($nombreVector[$j] > $nombreVector[$j+1]) {
+						$intercambio = $nombreVector[$j+1];
+						$nombreVector[$j+1]= $nombreVector[$j]; 
+						$nombreVector[$j] = $intercambio;
+					}  // fin del if
+				} // fin del for $j
+			} // fin del for $i
+			// devolver el vector ordenado
+			return $nombreVector;
+		} // fin de la función
 
 	?>
 
